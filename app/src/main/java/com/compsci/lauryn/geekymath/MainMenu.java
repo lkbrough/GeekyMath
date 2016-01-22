6a4f9eb737ae772e8fbea6da19dc97b270105bbd
fuzgirl1 <wolfpup1230@yahoo.com>
2016-01-22 14:23:42 -0600

    Comments and Removals of Uselessness
    Trying to get Learn working and retouching comments on this so it can be easier read.

--- app/src/main/java/com/compsci/lauryn/geekymath/MainMenu.java
+++ app/src/main/java/com/compsci/lauryn/geekymath/MainMenu.java
@@ -1,6 +1,6 @@
-package com.compsci.lauryn.geekymath;
+package com.compsci.lauryn.geekymath;//You can ignore this, it's just linking everything together and everything might explode if you remove it(JK)
 
-import android.content.Intent;
+import android.content.Intent;//These are just simple imports for android. MAKE SURE TO LEAVE THESE HERE!
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,24 +8,24 @@ import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity {//Bare nessesity to Extend some sort of Activity
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//Creates the initial look of the Activity (Activates XML Page)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) {//This creates the little bar at the top, this class really has no use for it but leave it here anyway so design is consistant
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {//Ignore this, this too is for that Little bar at the top.
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -39,36 +39,18 @@ public class MainMenu extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /*public void load1(View view){ //Orignal Space Waste Prototype for loading...
-        Intent intent=new Intent(this, Test.class);
-        intent.putExtra(message, "1");
-        startActivity(intent);
-    }
-
-    public void load2(View view){
-        Intent intent=new Intent(this, Test.class);
-        intent.putExtra(message, "2");
-        startActivity(intent);
-    }
-
-    public void load3(View view){
-        Intent intent=new Intent(this, Test.class);
-        intent.putExtra(message, "3");
-        startActivity(intent);
-    }*/
-
-    public void load(View view){
-        Intent intent=new Intent(this, Test.class);
-        /*if(view.getId()==R.id.learn)
-            intent=new Intent(this, Learn.class);*/
-        if(view.getId()==R.id.l3)
+    public void load(View view){//The only method I needed to write to make it work, I personally think I did pretty good job JK I tried
+        Intent intent=new Intent(this, Test.class);//Pass between MainMenu and Test Activities
+        if(view.getId()==R.id.learn)
+            intent=new Intent(this, Learn.class);//Change it just because we need to access Learn instead
+        else if(view.getId()==R.id.l3)//Set Global Variables
             Test.level=3;
-        else if(view.getId()==R.id.l2)
+        else if(view.getId()==R.id.l2)//Ditto
             Test.level=2;
         else
-            Test.level=1;
-        Test.quest=true;
-        startActivity(intent);
+            Test.level=1;//Ditto as well as a best error send
+        Test.quest=true;//Just to generate a new question in Test
+        startActivity(intent);//Onto Test! (or possibly Learn)
     }
 
 }
