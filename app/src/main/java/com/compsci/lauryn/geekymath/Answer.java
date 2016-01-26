5b27f9feaaa141c78ac6e5218b0c2748af8a29a0
fuzgirl1 <wolfpup1230@yahoo.com>
2016-01-25 21:41:56 -0600

    Comments
    Added Comments so it can easier be read

--- app/src/main/java/com/compsci/lauryn/geekymath/Answer.java
+++ app/src/main/java/com/compsci/lauryn/geekymath/Answer.java
@@ -1,6 +1,6 @@
-package com.compsci.lauryn.geekymath;
+package com.compsci.lauryn.geekymath;//Ignore this, links the project together.
 
-import android.content.Intent;
+import android.content.Intent;//Ignore the imports, only brings the methods and classes that are being used.
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,36 +9,36 @@ import android.view.View;
 import android.widget.TextView;
 
 
-public class Answer extends ActionBarActivity {
+public class Answer extends ActionBarActivity {//Basics extend something and because this uses the ActionBar, it made since.
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//Activated as soon as the Activity begins
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_answer);
-        Intent intent=getIntent();
-        String correct=intent.getStringExtra(Test.correct);
+        setContentView(R.layout.activity_answer);//Sets the view to the specifications provided by XML document
+        Intent intent=getIntent();//Retrieving the information put in the intent from the Test aka Correct or not Correct
+        String correct=intent.getStringExtra(Test.correct);//Storing the information
 
-        TextView textView=(TextView) findViewById(R.id.response);
-        textView.setTextSize(40);
-        textView.setText(correct);
+        TextView textView=(TextView) findViewById(R.id.response);//Retrieving the textview with pointless information
+        textView.setTextSize(40);//Makes it bigger because the activity is blank except for this and the button
+        textView.setText(correct);//Setting it to the proper reasponse
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) {//For now ignore this, this comment will be removed once I explain this more IRL
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_answer, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {//Same as Above
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if(id==R.id.main_menu){
+        if(id==R.id.main_menu){//Adds the button to go to MainMenu and clears streak
             Test test=new Test();
             test.stop();
             Intent intent=new Intent(this,MainMenu.class);
@@ -46,14 +46,14 @@ public class Answer extends ActionBarActivity {
             return true;
         }
         //noinspection SimplifiableIfStatement
-        else if (id == R.id.action_settings) {
+        else if (id == R.id.action_settings) {//Pointless for now
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void cont(View view){
+    public void cont(View view){//Basic Method only to return to Test Activity and generate a new Question
         Intent intent=new Intent(this, Test.class);
         Test.quest=true;
         startActivity(intent);
